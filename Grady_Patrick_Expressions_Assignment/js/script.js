332b9d567f2aee35b1bd9fa43f54dace85cce26f
prgrady <prgrady@fullsail.edu>
2015-01-12 17:12:07 -0500

    First Draft
    created first draft of pay calculator

--- Grady_Patrick_Expressions_Assignment/js/script.js
+++ Grady_Patrick_Expressions_Assignment/js/script.js
@@ -5,4 +5,11 @@
  */
 
 
-alert('Works');
+var start = prompt("What time did you start working today?");  // Get start time
+var end = prompt("What time did you stop working today?");  // Get end time
+var hrly = prompt("How much do you make per hour");  //Get hourly rate
+
+var tothrs = (end - start) / 100 - 0.5;  //Get total hours worked by subtracting end from start and half hour break
+var pay = tothrs * hrly;  //Pay is total hours worked times pay rate
+
+console.log("Total net pay for today is : $" + pay);
